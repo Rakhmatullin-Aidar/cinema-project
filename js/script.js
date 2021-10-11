33af9cd48674688c8e0d8bc7a60f20a184a80f3a
Rakhmatullin-Aidar <92167862+Rakhmatullin-Aidar@users.noreply.github.com>
2021-10-11 14:06:10 +0300

    Урок 3

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const schemeSvg = document.querySelector(".scheme-svg")
+const totalPriceTag = document.querySelector(".price-total")
+let price = 800;
+let totalPrice = 0;
+
+schemeSvg.addEventListener('click', (event)=>{
+    if(!event.target.classList.contains('booked')){
+        event.target.classList.toggle('active')
+        totalSeats = (schemeSvg.querySelectorAll(".active").length);
+        totalPrice = totalSeats * price;
+        totalPriceTag.textContent = totalPrice;
+    }    
+})
